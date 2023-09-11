7fe11a0a7d5be403342f918b6b9ec3867cd71e5b
1y3eo <hyeo131@naver.com>
2023-09-11 21:23:45 +0900

    pooling bonus particle

--- Assets/Carrot Clicker/Scripts/BonusParticleManager.cs
+++ Assets/Carrot Clicker/Scripts/BonusParticleManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class BonusParticleManager : MonoBehaviour
 {
     [Header(" Elements ")]
     [SerializeField] private CarrotManager carrotManager;
     [SerializeField] private GameObject bonusParticlePrefab;
+
+    [Header(" Pooling ")]
+    private ObjectPool<GameObject> bonusParticlesPool;
 
     private void Awake()
     {
@@ -20,7 +24,7 @@ public class BonusParticleManager : MonoBehaviour
     }
     void Start()
     {
-        
+        bonusParticlesPool = new ObjectPool<GameObject>(CreateFunction, ActionOnGet, ActionOnRelease, ActionOnDestroy);
     }
 
     void Update()
@@ -28,12 +32,33 @@ public class BonusParticleManager : MonoBehaviour
         
     }
 
+    private GameObject CreateFunction()
+    {
+        return Instantiate(bonusParticlePrefab, transform);
+    }
+
+    private void ActionOnGet(GameObject bonusParticle)
+    {
+        bonusParticle.SetActive(true);
+    }
+
+    private void ActionOnRelease(GameObject bonusParticle)
+    {
+        bonusParticle.SetActive(false);
+    }
+
+    private void ActionOnDestroy(GameObject bonusParticle)
+    {
+        Destroy(bonusParticle);
+    }
+
     private void CarrotClickedCallBack(Vector2 clickedPosition)
     {
-        GameObject bonusParticleInstance = Instantiate(bonusParticlePrefab, clickedPosition, Quaternion.identity, transform);
+        GameObject bonusParticleInstance = bonusParticlesPool.Get();
 
-        bonusParticleInstance.GetComponent<BonusParticle>().Configure(carrotManager.GetCurrentMultiplier()); ;
-        
-        Destroy(bonusParticleInstance, 1);
+        bonusParticleInstance.transform.position = clickedPosition;
+        bonusParticleInstance.GetComponent<BonusParticle>().Configure(carrotManager.GetCurrentMultiplier());
+
+        LeanTween.delayedCall(1, () => bonusParticlesPool.Release(bonusParticleInstance));
     }
 }
